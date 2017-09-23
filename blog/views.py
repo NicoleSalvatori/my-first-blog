b321840149fcd502d44b256c8006ca281063476c
Nicole <salvatorinicole@gmail.com>
2017-09-23 14:30:17 +0200

    Ho inserito i miei posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    #estrazione dati (query) dai post
+    #lte di plublish zone sta per less than equal
+    #metto - davanti a publish date per invertire l'ordine ed ordinarli dal più recente al meno
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    return render(request, 'blog/post_list.html', {'posts':posts})

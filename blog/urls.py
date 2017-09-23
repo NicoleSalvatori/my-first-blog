d2175be1f53b031570cd758f965232cb6b1d714c
Nicole <salvatorinicole@gmail.com>
2017-09-23 17:20:05 +0200

    Added view and template for detailed blog post as well as CSS for the site.

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views #dalla cartella corrente importa le liste che sono state cre
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.post_detail, name='post_detail'), 
 ]
 #dire che la vista principale deve essere ad una determinata pagina e lo devoquindi definire qui negli url
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 from django.utils import timezone
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 def post_list(request):
@@ -9,3 +10,7 @@ def post_list(request):
     #metto - davanti a publish date per invertire l'ordine ed ordinarli dal più recente al meno
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        return render(request, 'blog/post_detail.html', {'articolo': post})
